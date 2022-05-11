779a9f24b01a97608cbe0072403890f5027f8901
Talhaaa99 <talha.toufeeq99@gmail.com>
2022-05-12 04:21:17 +0530

    initial commit

---
+++ src/components/Layout/index.js
@@ -0,0 +1,14 @@
+import './index.scss';
+import Sidebar from '../Sidebar';
+
+
+const Layout =  () => {
+        return (
+            <>
+                <Sidebar />
+            </>
+        )
+}
+
+
+export default Layout
