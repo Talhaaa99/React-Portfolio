de9c5a52e56d1f91a53dcc72957a6873d9ecd790
Talhaaa99 <talha.toufeeq99@gmail.com>
2022-05-17 20:38:33 +0530

    initial

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,20 @@
 import './index.scss';
 import Sidebar from '../Sidebar';
+import { Outlet } from 'react-router-dom';
 
 
 const Layout =  () => {
         return (
-            <>
+            <div classname='App'>
                 <Sidebar />
-            </>
+                <div className='page'>
+                    <span className='tags top-tags'>&lt;body&gt;</span>
+                    <Outlet />
+                    <span className='tags bottom-tags'>&lt;/body&gt;</span>
+                    <span className='tags bottom-tag-html'>&lt;/html&gt;</span>
+
+                </div>
+            </div>
         )
 }
 
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,15 +1,14 @@
-
 import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
 import LogoS from '../../assets/Images/logo-s.png';
 import LogoSubtitle from '../../assets/Images/logo_sub.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 
 const Sidebar = () => (
-
-        <div className='nav-bar'>
+     <div className='nav-bar'>
             <Link className='logo' to='/'>
                 <img src={LogoS} alt='Logo' ></img>
                 <img className='sub-logo' src={LogoSubtitle} alt='LogoSub' ></img>
@@ -25,7 +24,19 @@ const Sidebar = () => (
                     <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
                 </NavLink>
             </nav>
-        </div>
-    )
+            <ul>
+                <li>
+                    <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/talha-toufeeq-8207551b4/'>
+                        <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
+                    </a>
+                </li>
+                <li>
+                    <a target='_blank' rel='noreferrer' href='https://github.com/Talhaaa99'>
+                        <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
+                    </a>
+                </li>
+            </ul>
+    </div>
+)
 
 export default Sidebar;
